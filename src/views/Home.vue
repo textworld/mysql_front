ca6585cec085ec5bc5f55bb80a3ee14b5155f463
textworld <text.zwb@gmail.com>
2020-08-14 18:02:21 +0800

    for save

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,49 @@
 <template>
   <div class="home">
     <p>
-      count: {{ counta }},
-      username: {{ username }}
+      TODO： 实现多选删除功能
+      <el-button @click="">
+        删除
+      </el-button>
     </p>
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-table
+            :data="tableData"
+            style="width: 100%"
+            @selection-change="handleSelectionChange">
+      <el-table-column
+              type="selection"
+              width="55">
+      </el-table-column>
+      <el-table-column
+        prop="date"
+        label="日期">
+      </el-table-column>
+      <el-table-column
+              prop="addr"
+              label="地址">
+      </el-table-column>
+      <el-table-column
+              label="操作">
+        <template slot-scope="scope">
+          <el-button
+                  @click="remove(scope)"
+                  type="primary"
+                  size="small">
+            移除
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-dialog
+            title="提示"
+            :visible.sync="dialogVisible"
+            width="30%">
+      <span>这是一段信息</span>
+      <span slot="footer" class="dialog-footer">
+    <el-button @click="dialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="doRemove">确 定</el-button>
+  </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -15,6 +53,29 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
+  data(){
+    return {
+      tableData: [
+        {
+          id: 1,
+          date: '2020-01-02',
+          addr: '51'
+        },
+        {
+          id: 2,
+          date: '2020-01-03',
+          addr: '52'
+        },
+        {
+          id: 3,
+          date: '2020-01-04',
+          addr: '53'
+        }
+      ],
+      dialogVisible: false,
+      index: 0
+    }
+  },
   computed: {
     counta(){
       return this.$store.state.count
@@ -27,6 +88,18 @@ export default {
     HelloWorld
   },
   methods: {
+    remove(scope){
+      console.log(scope)
+      this.index = scope.$index
+      this.dialogVisible = true
+    },
+    doRemove() {
+      this.tableData.splice(this.index, 1)
+      this.dialogVisible = false
+    },
+    handleSelectionChange(val){
+      console.log(val)
+    }
   }
 }
 </script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -16,6 +16,9 @@
 </template>
 
 <script>
+  import aesjs from 'aes-js'
+  import CryptoJS from 'crypto-js'
+  import JSEncrypt from 'jsencrypt'
 export default {
   name: "Login",
   data() {
@@ -27,8 +30,34 @@ export default {
     }
   },
   methods: {
+    encrypt(password) {
+      var key = CryptoJS.enc.Hex.parse('31323334353637383930313233343536');
+      var iv = CryptoJS.enc.Hex.parse('0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f');
+      var encrypted = CryptoJS.AES.encrypt("text can be a random lenght", key, {
+        mode: CryptoJS.mode.CTR,
+        iv: iv,
+        padding: CryptoJS.pad.NoPadding
+      });
+      console.log(encrypted.ciphertext.toString())
+      //1831e01b304d16fb27ec48ba8c2b5cb5bf0e84c6a88fa4b118b348
+    },
+    rsaEncrypt() {
+      // Encrypt with the public key...
+      var encrypt = new JSEncrypt();
+      encrypt.setPublicKey("-----BEGIN PUBLIC KEY-----\n" +
+              "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDlOJu6TyygqxfWT7eLtGDwajtN\n" +
+              "FOb9I5XRb6khyfD1Yt3YiCgQWMNW649887VGJiGr/L5i2osbl8C9+WJTeucF+S76\n" +
+              "xFxdU6jE0NQ+Z+zEdhUTooNRaY5nZiu5PgDB0ED/ZKBUSLKL7eibMxZtMlUDHjm4\n" +
+              "gwQco1KRMDSmXSMkDwIDAQAB\n" +
+              "-----END PUBLIC KEY-----");
+      var encrypted = encrypt.encrypt('text can be a random lenght');
+      console.log(encrypted)
+      //kkEwvrhMsgN1dq+NVHjpiGEQR7dmXJQw2l++6zSF7BvIqjU2VzzyZBGD1MZD1muuxEG4+ERDdVn+aN+nXu4fqrrtHSdtMu/TR4oHlg6nkkSoPqZLuxvQShLp3zwBMqA5Bd2vnWt9MbSjgKSlckXJSQxan/RiRQbhSv2FS/iZwyY=
+    },
     login () {
       const userinfo = this.form
+      this.encrypt(this.form.password)
+      this.rsaEncrypt()
       this.$store.dispatch('login', userinfo).then(resp => {
         console.log(resp)
         this.$router.push({

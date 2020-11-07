bede69d63265f6f90e0e9075d5ea53c44d96c955
textworld <text.zwb@gmail.com>
2020-11-08 00:55:22 +0800

    add: login api and mock

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import { getToken, setToken, removeToken } from "@/utils/auth";
 import User from './modules/user'
-<<<<<<< HEAD
-
-=======
 import dbtables from "@/store/modules/dbtables";
->>>>>>> c085a8d04d63b920252423562040d7107f5698e9
 Vue.use(Vuex)
 // const store = new Vuex.Store({
 //   state: {
@@ -20,11 +16,7 @@ Vue.use(Vuex)
 // })
 export default new Vuex.Store({
   modules: {
-<<<<<<< HEAD
-    user: User
-=======
     user: User,
     dbLexicon: dbtables
->>>>>>> c085a8d04d63b920252423562040d7107f5698e9
   }
 })
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,13 +1,13 @@
 import JSEncrypt from 'jsencrypt'
 import {setToken, getToken, removeToken} from '@/utils/auth'
-import {logout} from '@/api/user'
+import {login, logout} from '@/api/user'
 const rasPublicKey = "-----BEGIN PUBLIC KEY-----\n" +
     "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDlOJu6TyygqxfWT7eLtGDwajtN\n" +
     "FOb9I5XRb6khyfD1Yt3YiCgQWMNW649887VGJiGr/L5i2osbl8C9+WJTeucF+S76\n" +
     "xFxdU6jE0NQ+Z+zEdhUTooNRaY5nZiu5PgDB0ED/ZKBUSLKL7eibMxZtMlUDHjm4\n" +
     "gwQco1KRMDSmXSMkDwIDAQAB\n" +
     "-----END PUBLIC KEY-----"
-
+    
 export default {
     state: () => ({
         username: getToken()

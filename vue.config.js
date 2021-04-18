9e8000abe02c0eadc7101e98657cc91c93829db3
textworld <text.zwb@gmail.com>
2021-04-18 09:28:22 +0800

    mysql安装任务列表

--- vue.config.js
+++ vue.config.js
@@ -22,16 +22,16 @@ module.exports = {
     devServer: {
         port: port,
         open: true,
-        // proxy: {
-        //     '/api': {
-        //         target: 'http://localhost:8010',
-        //         changeOrigin: true
-        //     },
-        // },
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8000',
+                changeOrigin: true
+            },
+        },
         overlay: {
             warnings: false,
             errors: true
         },
-        before: require('./mock/mock-server.js')
+        //before: require('./mock/mock-server.js')
     }
 }
